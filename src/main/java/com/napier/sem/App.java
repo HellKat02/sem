4696018ddff5538a852f26e68a51b61f1307bbbd
Nightmare <50259779+boo555@users.noreply.github.com>
2020-01-09 10:24:11 +0630

    Re-edit java file

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -38,6 +38,83 @@ import java.sql.*;
 public class App
 {
     public static void main(String[] args)
+//    {
+//        try
+//        {
+//            // Load Database driver
+//            Class.forName("com.mysql.jdbc.Driver");
+//        }
+//        catch (ClassNotFoundException e)
+//        {
+//            System.out.println("Could not load SQL driver");
+//            System.exit(-1);
+//        }
+//
+//        // Connection to the database
+//        Connection con = null;
+//        int retries = 100;
+//        for (int i = 0; i < retries; ++i)
+//        {
+//            System.out.println("Connecting to database...");
+//            try
+//            {
+//                // Wait a bit for db to start
+//                //Thread.sleep(30000);
+//                // Connect to database
+//                con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
+//                System.out.println("Successfully connected");
+//                // Wait a bit
+//
+//                // Exit for loop
+//                break;
+//            }
+//            catch (SQLException sqle)
+//            {
+//                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+//                System.out.println(sqle.getMessage());
+//                try{
+//                    Thread.sleep(30000);
+//                }
+//                catch (InterruptedException ie)
+//                {
+//                    System.out.println("Thread interrupted? Should not happen.");
+//                }
+//            }
+//
+//        }
+//
+//        if (con != null)
+//        {
+//            try
+//            {
+//                // Close connection
+//                con.close();
+//            }
+//            catch (Exception e)
+//            {
+//                System.out.println("Error closing connection to database");
+//            }
+//        }
+//    }
+    {
+        // Create new Application
+        App a = new App();
+
+        // Connect to database
+        a.connect();
+
+        // Disconnect from database
+        a.disconnect();
+    }
+    /**
+     * Connection to MySQL database.
+     */
+    private Connection con = null;
+
+    /**
+     * Connect to the MySQL database.
+     */
+    public void connect()
     {
         try
         {
@@ -50,39 +127,36 @@ public class App
             System.exit(-1);
         }
 
-        // Connection to the database
-        Connection con = null;
-        int retries = 100;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
             try
             {
                 // Wait a bit for db to start
-                //Thread.sleep(30000);
+                Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
-                // Wait a bit
-
-                // Exit for loop
                 break;
             }
             catch (SQLException sqle)
             {
                 System.out.println("Failed to connect to database attempt " + Integer.toString(i));
                 System.out.println(sqle.getMessage());
-                try{
-                    Thread.sleep(30000);
-                }
-                catch (InterruptedException ie)
-                {
-                    System.out.println("Thread interrupted? Should not happen.");
-                }
             }
-
+            catch (InterruptedException ie)
+            {
+                System.out.println("Thread interrupted? Should not happen.");
+            }
         }
+    }
 
+    /**
+     * Disconnect from the MySQL database.
+     */
+    public void disconnect()
+    {
         if (con != null)
         {
             try
