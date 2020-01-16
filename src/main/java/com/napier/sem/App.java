6dbadd2b2e9a5dce6eeb45ceb44bbff40d899779
Nightmare <nightmare51379@gmail.com>
2020-01-16 10:43:23 +0630

    display employees salaries information.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -275,7 +275,7 @@ public class App
         catch (Exception e)
         {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get salary details");
+            System.out.println("Failed to get salary details about employees");
             return null;
         }
     }
